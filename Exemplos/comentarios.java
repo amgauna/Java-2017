4360df72da455be1bb2e0b6eed24e8987755b615
Ana Mercedes Gauna <amgauna@gmail.com>
2017-04-14 17:34:55 -0300

    Create comentarios.java

---
+++ Exemplos/comentarios.java
@@ -0,0 +1,20 @@
+/*
+ * Este código é um pouco complicado...
+ * Vou tentar explicá-lo aqui:
+ */
+ 
+/**
+  * Esta classe faz coisas inclivelmente maravilhosas e todos que querem tornar seus applets melhores devem, 
+  * sem dúvida, usar sua subclasse.
+  */
+  
+ class CoolApplet extends Applet {
+  
+ /**
+   * Este métido recebe dois parâmetros:
+   * @param key e o nome do simbolo a ser armazenado.
+   * @param value e aquilo que deve ser armazenado associado a esta tecla.
+   */
+   
+   void put (String key, Object value) {
+   
