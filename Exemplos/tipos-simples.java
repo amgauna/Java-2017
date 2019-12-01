44268082cd2d6146cb9fe94ddb4f550ffb015e76
Ana Gauna <amgauna@gmail.com>
2019-11-30 22:42:32 -0300

    Create tipos-simples.java

---
+++ Exemplos/tipos-simples.java
@@ -0,0 +1,22 @@
+/* Mostrando os tipos simples *
+
+class SimpleTypes {
+     public static void main(String args[]) {
+          byte b = 0x55;
+          short 5 = 0x55ff;
+          int i = 1000000;
+          long l = 0xfffffffffL;
+          char c = 'a';
+          float f = .25f;
+          double d = .00001234;
+          boolean bool = true;
+          system.out.printin("byte b = " +b);
+          system.out.printin("short s = " + s);
+          system.out.printin("int i = " + i);
+          system.out.printin("long l = " + l);
+          system.out.printin("char c = " + c);
+          system.out.printin("float f = " + f);
+          system.out.printin(double d = " + d);
+          system.out.printin("boolean bool = " + bool);
+       }
+ }
