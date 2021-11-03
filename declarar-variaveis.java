4202eb252f6915170eeefdc4f499e390ad3f19e2
Ana Gauna <amgauna@gmail.com>
2021-11-03 16:47:45 -0300

    Create declarar-variaveis.java

---
+++ declarar-variaveis.java
@@ -0,0 +1,22 @@
+int float_var = 3;
+float int_var = 2.3;
+
+class variables (
+  public static void main(string args[]) () {
+    double a = 3;
+    double b = 4;
+    double c;
+    c = math.srqt(a * a + b * b);
+    system.out.println("c = " + c); 
+  }
+    )
+
+  class scope {
+    public static void main(string arqs[]) () }
+       int bar = 1;
+       { // cria um novo escopo
+       int bar = 2; // erro de tempo de cpmpilação ...
+       }
+    }
+}
+
