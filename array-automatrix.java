9bf86177b480af86be11df237aabd38308e0ab54
Ana Gauna <amgauna@gmail.com>
2021-11-03 18:35:24 -0300

    Create array-automatrix.java

---
+++ array-automatrix.java
@@ -0,0 +1,16 @@
+class AutoMatrix {
+  public static void main(string args[]) {
+    
+    double m[] [] = {
+      { 0*0, 1*0, 2*0, 3*0 ),
+      { 0*1, 1*1, 2*1, 3*1 ),
+      { 0*2, 1*2, 2*2, 3*2 ),
+      { 0*3, 1*3, 2*3, 3*3 ),
+    };
+       
+    system.out.println(m[0][0] +" "+ m[0] [1] +" "+ m[0] [2] +" "+ m[0] [3];
+    system.out.println(m[1][0] +" "+ m[1] [1] +" "+ m[1] [2] +" "+ m[1] [3];
+    system.out.println(m[2][0] +" "+ m[2] [1] +" "+ m[2] [2] +" "+ m[2] [3];                   
+    system.out.println(m[3][0] +" "+ m[3] [1] +" "+ m[3] [2] +" "+ m[3] [3];                   
+    };                  
+                       
