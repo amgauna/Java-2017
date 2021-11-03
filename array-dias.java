ef213e09e163a534243ed539133c216c8727411c
Ana Gauna <amgauna@gmail.com>
2021-11-03 17:15:37 -0300

    Create array-dias.java

---
+++ array-dias.java
@@ -0,0 +1,32 @@
+/* array simples */
+
+
+class array {
+  public static void main(string args[]) {
+    int dias_mes[];
+    dias_mes = new int[12];
+    dias_mes[0] = 31;
+    dias_mes[1] = 28;
+    dias_mes[2] = 31;
+    dias_mes[3] = 30;
+    dias_mes[4] = 31;
+    dias_mes[5] = 30;
+    dias_mes[6] = 31;
+    dias_mes[7] = 31;
+    dias_mes[8] = 30;
+    dias_mes[9] = 31;
+    dias_mes[10] = 30;
+    dias_mes[11] = 31;
+    system.out.println("Abril tem " + dias_mes[3] + "dias.");
+  }
+}
+
+/* array automático */
+
+class AutoArray {
+  public static void main(string args[]) {
+    int dias_mes[] = { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 );
+    system.out.println("Abril tem " + dias_mes[3] + "dias";
+    }
+ }
+                      
