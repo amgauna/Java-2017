edb1c1843665da393e48ce62d85f4d88a22e4217
Ana Gauna <amgauna@gmail.com>
2021-11-03 17:01:42 -0300

    Update declarar-variaveis.java

--- declarar-variaveis.java
+++ declarar-variaveis.java
@@ -1,3 +1,5 @@
+// variaveis
+
 int float_var = 3;
 float int_var = 2.3;
 
@@ -20,3 +22,36 @@ class variables (
     }
 }
 
+
+class promote {
+  public static void main(strings[]) {
+    byte b = 42;
+    char c = 'a';
+    short s = 1024;
+    int i = 50000;
+    float f = 5.67f;
+    double d = .1234;
+    double result = (f * b) +(i / c) - (d * s);
+    system.out.println((f + b) + "+" +(i / c) + "-" + (d * s));
+    system.out.println("resultado = "+ result");
+    }
+}
+
+// caracteres
+                       
+char c;
+char c2 = cxf132;
+char c3 = 'a';
+char c4 = '\n';
+int tres = 3;
+char um = '1';
+char quatro = (char) (tres + um);
+                       
+// boolean
+                       
+boolean done = false;
+                       
+                       
+                       
+                       
+                       
